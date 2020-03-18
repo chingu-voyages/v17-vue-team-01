10c55985a7076fe7ddf244c8ad76ecab24dc4bbf
Leigh Kristine Roxas-Mobley <lkroxmob@gmail.com>
2020-03-18 01:49:45 -0700

    added password visibility functionality

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -3,11 +3,38 @@
     <v-text-field
       v-model="username"
       :counter="15"
-      :rules="[v => !!v || 'Enter username']"
+      :rules="[v => !!v || 'Enter username (must be less than 15 characters)']"
       label="username"
       required
     ></v-text-field>
 
-    <v-text-field v-model="password" :rules="passwordRules" label="password" required></v-text-field>
+    <v-text-field
+      v-model="password"
+      :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+      :rules="[rules.required, rules.min]"
+      :type="show1 ? 'text' : 'password'"
+      name="input=10-1"
+      label="password"
+      counter
+      @click:append="show1 = !show1"
+    ></v-text-field>
+
+    <v-btn @click="login">login</v-btn>
   </v-form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      show1: false,
+      password: "Password",
+      rules: {
+        required: value => !!value || "Required.",
+        min: v => v.length >= 15 || "Minimum 15 characters"
+      }
+    };
+  }
+};
+</script>
+
