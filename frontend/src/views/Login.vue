7f92c2f7871c866451c06d91c4a0795e16acc7ec
Leigh Kristine Roxas-Mobley <lkroxmob@gmail.com>
2020-03-18 01:03:52 -0700

    initial login form setup

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-form ref="form" v-model="valid" lazy-validation>
+    <v-text-field
+      v-model="username"
+      :counter="15"
+      :rules="[v => !!v || 'Enter username']"
+      label="username"
+      required
+    ></v-text-field>
+
+    <v-text-field v-model="password" :rules="passwordRules" label="password" required></v-text-field>
+  </v-form>
+</template>
