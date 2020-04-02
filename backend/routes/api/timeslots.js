8f3e0b605278788e99062397ddb50087c0f69feb
ALLY <abinding@gmail.com>
2020-04-02 17:30:14 -0500

    update timeslots.js

--- backend/routes/api/timeslots.js
+++ backend/routes/api/timeslots.js
@@ -19,50 +19,124 @@ router.post('/create', (req, res) => {
     if (!token) return res.status(401).send({ success: false, message: 'No token provided.' });
     
     jwt.verify(token, key, function(err, decoded) {
-      if (err) return res.status(500).send({ success: false, message: 'Failed to authenticate token.' });
+      if (err) return res.status(500).send({ success: false, message: 'Failed to authenticate token.' })});
   
-      let timeslots = res.body.timeslots;
       let {
         event_id,
-        day,
-        time,
+        timeslots
       } = req.body;
       const user_id = decoded._id;
+
       timeslots.forEach(timeslot => {
           let newTimeslot = new Timeslot({
+            users: [user_id],
+            events: [event_id],
             _id: timeslot_id,
             day,
             time,
           });
-          newTimeslot.save();
-          return res.status(201).json({
+
+          newTimeslot.save().then(timeslot => {
+
+            // User.findOneAndUpdate({
+            //   _id: user_id
+            // },
+            // { timeslots: [timeslot._id] },function(err, doc){
+            //   if(err){
+            //       console.log("Something wrong when updating data!");
+            //   }
+            //   console.log("Added this timeslot to user's profile!")
+              
+            //   return res.status(201).json({
+            //     success: true,
+            //     msg: "Congrats, " + timeslot_id + " timeslot_id is created for user_id of " + doc._id
+            // });
+            // });  
+
+        User.findOneAndUpdate({
+          user_id: user_id,
+          timeslots: {$ne: timeslot_id}
+        },
+        { $push: {timeslots: [timeslot_id] }} ,function(err, doc){
+          if(err){
+              console.log("Something wrong when updating data!");
+          }
+          doc ? console.log("Added this timeslot to user!") : console.log("This timeslot is already in this user!");
+        });
+    
+        Event.findOneAndUpdate({
+          _id: event_id,
+          timeslots: {$ne: timeslot_id}
+        },
+        { $push: {timeslots: timeslot_id }},function(err, doc){
+          if(err){
+              console.log("Something wrong when updating data!");
+          }
+          doc ? console.log("Added this timeslot to event!") : console.log("This timeslot is already in this event!");
+          return res.status(200).json({
             success: true,
-            msg: "Congrats, timeslot associated with " + event._id + " and user " + user_id
-        });  
-      });
-      
-          
+            msg: "Congrats, timeslot_id " + timeslot_id + " is in event " + event_id
+        });
+        });
     });   
   });
-
-/**
- * @route GET api/timeslots/show
- * @desc Shows timeslots for event
- * @access Public
- */
-router.get('/show', (req, res) => {
-    let event_id = req.body.event_id;
-    //console.log(event_id);
-    Timeslot.find( {event: event_id}  ).then((result) => {
-  
-      if (!result) {
-        return res.status(400).json({
-          success: false,
-          msg: "Event not found."
-        });
-      }
-      res.status(200).send(result);
-    })   
 });
 
+
+/**
+ * @route POST api/timeslots/delete
+ * @desc Delete timeslot
+ * @access Public
+ */
+router.post('/delete', (req, res) => {
+  let timeslot_id = req.body.timeslot_id;
+  //console.log(timeslot_id);
+  Timeslot.findOne( {_id: timeslot_id}  ).then((result) => {
+    if(!result){
+      return res.status(200).json({
+        success: false,
+        msg: "Timeslot not found!"
+    });
+    }
+    let users = result.users;
+    users.forEach(user =>
+      User.findOneAndUpdate({
+        _id: user._id,
+        timeslots: {$eq: timeslot_id}
+      },
+      { $pull: {timeslots: timeslot_id }} ,function(err, doc){
+        if(err){
+            console.log("Something wrong when updating data!");
+        }
+        doc ? console.log("Removed this timeslot from user!") : console.log("Timeslot not in this user!");
+      }));
+
+      let events = result.events;
+      events.forEach(event =>
+        Event.findOneAndUpdate({
+          _id: event_id,
+          timeslots: {$eq: timeslot_id}
+        },
+        { $pull: {timeslots: timeslot_id }} ,function(err, doc){
+          if(err){
+              console.log("Something wrong when updating data!");
+          }
+          doc ? console.log("Removed this timeslot from event!") : console.log("Timeslot not in this event!");
+        }));
+
+      Timeslot.findOneAndDelete( {_id: timeslot_id}  ).then((result) => {
+        res.status(200).json({
+          success: true,
+          msg: "Congrats, timeslot_id " + result.timeslot_id + " is deleted and removed from each user"
+      });
+      }) 
+
+  });
+    
+});
+
+
 module.exports = router;
+
+
+
