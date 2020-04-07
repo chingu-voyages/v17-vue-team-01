49d1f7c4e7a8be40ddaa5d861f389f2040dd9128
alexever17@gmail.com <alexever17@gmail.com>
2020-04-07 04:44:28 +0200

    update in createEvent to account for route changes

--- frontend/src/views/Event.vue
+++ frontend/src/views/Event.vue
@@ -1,7 +1,10 @@
 <template>
     <v-sheet>
         <p>Route {{ url }} <v-btn to="/" color="primary">Home</v-btn></p>
-        <p>{{ event }}</p>
+        <p>Event</p>
+        <p>{{ userPart }}</p>
+        <p>Timeslots</p>
+        <p>{{ timeslotPart }}</p>
     </v-sheet>
 </template>
 
@@ -9,24 +12,32 @@
 export default {
     name: "Event",
     created() {
-        // this.url = this.$route.params.id;
-        this.url = "5e879d27148d8d6cb087dd2f";
+        this.url = this.$route.params.id;
+    },
+    watch: {
+        event: function(newer, older) {
+            if (newer) {
+                this.userPart = this.event[0];
+                this.timeslotPart = this.event[1]
+            }
+        }
     },
     data: () => ({
         url: "",
         event: null,
-        answer: null
+        answer: null,
+        userPart: null,
+        timeslotPart: null
     }),
     mounted() {
-        console.log(
             this.axios
-                .get(`http://34.82.150.138:5000/api/events/show/${this.url}`)
+                .get(`http://34.82.150.138:5000/api/events/show/${this.url}`, {
+                    headers: { "x-access-token": localStorage.getItem("token") }
+                })
                 .then(
                     response => (this.event = response.data),
-                    console.log(this.event)
                 )
                 .catch(error => (console.log(error), (this.answer = error)))
-        );
     }
 };
 </script>
