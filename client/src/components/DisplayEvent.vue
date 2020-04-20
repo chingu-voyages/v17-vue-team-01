295f1836ffe2562b0b75d4aa805d2e0e15314fc5
MarcosMe <marcosmendes@hotmail.com>
2020-04-20 22:00:39 +0100

    display timeslots for all users

--- client/src/components/DisplayEvent.vue
+++ client/src/components/DisplayEvent.vue
@@ -44,7 +44,7 @@
                       :key="`checkbox-label-${i}-${n}`"
                       :for="`checkbox-${i}-${n}`"
                     >
-                     <span :style="{ background: computedClass(i, n, eventPart.color) }">{{numbering(n-1)}}:00</span>
+                     <span :style="{ background: computedClass(day, n-1, eventPart.color, eventPart.users.length ) }">{{numbering(n-1)}}:00</span>
                       <input
                         :key="`checkbox-${i}-${n}`"
                         type="checkbox"
@@ -67,7 +67,7 @@
 
 
 
-      <v-list :shaped ="shaped">
+      <!--<v-list :shaped ="shaped">
       <div>
         <v-row>
         <span class="mx-8 mb-8" :inactive="inactive" v-for="(participants, i) in eventPart.users" :key="i">
@@ -78,7 +78,7 @@
         </span>  
         </v-row>    
       </div>
-      </v-list>
+      </v-list>-->
      
       <!-- <v-list :shaped="shaped">
       <v-list-item-group v-model="event" color="primary">
@@ -119,23 +119,62 @@ export default {
     
   },
   methods: {
+
+  pSBC: function(p,c0,c1,l){
+	let r,g,b,P,f,t,h,i=parseInt,m=Math.round,a=typeof(c1)=="string";
+	if(typeof(p)!="number"||p<-1||p>1||typeof(c0)!="string"||(c0[0]!='r'&&c0[0]!='#')||(c1&&!a))return null;
+	if(!this.pSBCr)this.pSBCr=(d)=>{
+		let n=d.length,x={};
+		if(n>9){
+			[r,g,b,a]=d=d.split(","),n=d.length;
+			if(n<3||n>4)return null;
+			x.r=i(r[3]=="a"?r.slice(5):r.slice(4)),x.g=i(g),x.b=i(b),x.a=a?parseFloat(a):-1
+		}else{
+			if(n==8||n==6||n<4)return null;
+			if(n<6)d="#"+d[1]+d[1]+d[2]+d[2]+d[3]+d[3]+(n>4?d[4]+d[4]:"");
+			d=i(d.slice(1),16);
+			if(n==9||n==5)x.r=d>>24&255,x.g=d>>16&255,x.b=d>>8&255,x.a=m((d&255)/0.255)/1000;
+			else x.r=d>>16,x.g=d>>8&255,x.b=d&255,x.a=-1
+		}return x};
+	h=c0.length>9,h=a?c1.length>9?true:c1=="c"?!h:false:h,f=this.pSBCr(c0),P=p<0,t=c1&&c1!="c"?this.pSBCr(c1):P?{r:0,g:0,b:0,a:-1}:{r:255,g:255,b:255,a:-1},p=P?p*-1:p,P=1-p;
+	if(!f||!t)return null;
+	if(l)r=m(P*f.r+p*t.r),g=m(P*f.g+p*t.g),b=m(P*f.b+p*t.b);
+	else r=m((P*f.r**2+p*t.r**2)**0.5),g=m((P*f.g**2+p*t.g**2)**0.5),b=m((P*f.b**2+p*t.b**2)**0.5);
+	a=f.a,t=t.a,f=a>=0||t>=0,a=f?a<0?t:t<0?a:a*P+t*p:0;
+	if(h)return"rgb"+(f?"a(":"(")+r+","+g+","+b+(f?","+m(a*1000)/1000:"")+")";
+	else return"#"+(4294967296+r*16777216+g*65536+b*256+(f?m(a*255):0)).toString(16).slice(1,f?undefined:-2)
+},
+
     numbering(n) {
       if (n < 10) {
         return `0${n}`;
       }
       return n;
     },
-    computedClass(i, n, c) {
-      // More complicated logic to determine what
-      // class should be applied
-      if(i == 0 && n == 3){
-        c += 10;
+    computedClass(i, n, c, nUser) {
+      const colorBase = this.pSBC(0.9,c,false,true);
+      //console.log(colorBase);
+      let nUsers = (0.9/nUser);
+      let count = 0;
+      this.changeToUserTZ.forEach(function (tmsl){ 
+        if(i == tmsl.day && n == tmsl.time){
+          count++;
+          console.log(count);
+          console.log(i);
+          console.log(n);
+          console.log(tmsl.day);
+          console.log(tmsl.time);
+        }
+      })
+      
+      if(count != 0){
+        let color = this.pSBC(0.9-(nUsers*count),c,false,true);
+        return color;
       }
-      if(i == 1 && n == 2){
-        c += 20;
+      else{
+        return colorBase;
       }
-      console.log(c);
-      return c;
+      
     }
   },
   computed: {
