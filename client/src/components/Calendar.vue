1b750bd867a13c4a87c274f81588646b18ec0e9e
ALLY <abinding@gmail.com>
2020-05-01 14:37:02 -0500

    resolve date display issue on Calendar

--- client/src/components/Calendar.vue
+++ client/src/components/Calendar.vue
@@ -16,23 +16,13 @@
             }}
           </v-toolbar-title>
           <v-spacer></v-spacer>
-
           <v-menu bottom right>
             <template v-slot:activator="{ on }">
-              <v-toolbar-title class="hidden-sm-and-up">
-                <v-btn icon v-on="on">
-                  <v-icon>mdi-dots-vertical</v-icon>
-                </v-btn>
-              </v-toolbar-title>
-
-              <v-toolbar-title class="hidden-xs-only">
-                <v-btn outlined color="grey darken-2" v-on="on">
-                  <span>{{ typeToLabel[type] }}</span>
-                  <v-icon right>mdi-menu-down</v-icon>
-                </v-btn>
-              </v-toolbar-title>
+              <v-btn outlined color="grey darken-2" v-on="on">
+                <span>{{ typeToLabel[type] }}</span>
+                <v-icon right>mdi-menu-down</v-icon>
+              </v-btn>
             </template>
-
             <v-list>
               <v-list-item @click="type = 'day'">
                 <v-list-item-title>Day</v-list-item-title>
@@ -50,7 +40,6 @@
           </v-menu>
         </v-toolbar>
       </v-sheet>
-
       <v-sheet height="70vh" class="calendarRows">
         <v-calendar
           ref="calendar"
@@ -76,20 +65,22 @@
               <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
             </v-toolbar>
             <v-card-text>
-              <h3>{{selectedEvent.details}}</h3>
-              <br>
-              <h4>Start: {{selectedEvent.start}}</h4>
-              <h4>End: {{selectedEvent.end}}</h4>
-              <br>
-              <h4>Users:</h4>
-              <template v-for="user in selectedEvent.users">
-                <h4 :key="user">{{ user }}</h4>
-              </template>
+              <v-list :shaped="shaped" max-height="35vh" class="overflow-y-auto">
+                <v-list-item-group color="primary">
+                  
+                  <template v-for="user in selectedEvent.users">
+                    <v-list-item :inactive="inactive" :key="user">
+                      <v-list-item-content class="text-left">
+                        <v-list-item-title>{{ user }}</v-list-item-title>
+                        <v-list-item-subtitle></v-list-item-subtitle>
+                      </v-list-item-content>
+                    </v-list-item>
+                  </template>
+                </v-list-item-group>
+              </v-list>
             </v-card-text>
-
             <v-card-actions>
-              <v-btn color="primary" :to="eventLink(selectedEvent.id)">View</v-btn>
-              <v-btn color="danger" @click="selectedOpen = false">Close</v-btn>
+              <v-btn text color="secondary" @click="selectedOpen = false">Cancel</v-btn>
             </v-card-actions>
           </v-card>
         </v-menu>
@@ -129,8 +120,6 @@ export default {
     ]
   }),
   mounted() {
-    this.today = this.getTodayDate();
-    this.value = this.today;
     if (this.usertoken) {
       this.axios
         .get("https://chingutime.herokuapp.com/api/users/profile", {
@@ -148,6 +137,8 @@ export default {
   },
   methods: {
     setup() {
+      this.today = this.getTodayDate();
+      this.value = this.today;
       for (let i = 0; i < this.user.events.length; i++) {
         if (this.user.events[i].scheduled == true) {
           this.eventIDs.push(this.user.events[i]["_id"]);
@@ -156,9 +147,7 @@ export default {
       for (let j = 0; j < this.eventIDs.length; j++) {
         this.axios
           .get(
-            `https://chingutime.herokuapp.com/api/events/show/${
-              this.eventIDs[j]
-            }`,
+            `https://chingutime.herokuapp.com/api/events/show/${this.eventIDs[j]}`,
             {
               //.get(`http://localhost:5000/api/events/show/${this.url}`, {
               headers: {
@@ -179,9 +168,9 @@ export default {
     title(value) {
       let dateArray = value.split("-");
       var date = new Date(
-        Date.UTC(dateArray[0], dateArray[1] - 1, dateArray[2])
+        Date.UTC(dateArray[0], dateArray[1], dateArray[2])
       );
-      const month = date.toLocaleString("en-US", { month: "short" });
+      const month = date.toLocaleString("en-US", { month: "long" });
       return `${month} ${dateArray[0]}`;
     },
     viewDay({ date }) {
@@ -218,10 +207,9 @@ export default {
       if (this.eventIDs.length == this.serverResponse.length) {
         for (let i = 0; i < this.serverResponse.length; i++) {
           this.events.push({
-            id: this.serverResponse[i][0]["_id"],
             color: this.serverResponse[i][0].color,
             end: this.transformTimestamp(this.serverResponse[i][0].end),
-            name: this.serverResponse[i][0].title,
+            title: this.serverResponse[i][0].title,
             details: this.serverResponse[i][0].details,
             start: this.transformTimestamp(this.serverResponse[i][0].start),
             users: this.transformUsers(this.serverResponse[i][0].users)
@@ -238,9 +226,6 @@ export default {
         result.push(input[i].username);
       }
       return result;
-    },
-    eventLink(id) {
-      return "/event/" + id;
     }
   }
 };
@@ -251,6 +236,6 @@ export default {
   margin-top: 8px;
 }
 .calendarTitle {
-  font-size: 18px;
+  margin-left: 5px;
 }
 </style>
