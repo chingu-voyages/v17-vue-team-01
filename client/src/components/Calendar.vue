9e865fe284403b1bd812276f35194a75351fbc1f
MarcosMe <marcosmendes@hotmail.com>
2020-05-01 22:40:23 +0100

    correct #79 with fringe cases

--- client/src/components/Calendar.vue
+++ client/src/components/Calendar.vue
@@ -244,7 +244,7 @@ export default {
         day = newDate.getFullYear() + '-' + ("0" + (newDate.getMonth() + 1)).slice(-2) + '-' + ("0" + (newDate.getDate())).slice(-2); 
         time = time - 24;
       }
-      //console.log(input.slice(0, 10) + " " + time + ":00");
+      //console.log(day + " " + time + ":00");
       return day + " " + time + ":00";
     },
     transformUsers(input) {
